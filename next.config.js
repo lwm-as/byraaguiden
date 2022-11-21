6680105b2c4af342c7b0a614866d9b76033afc95
Mabero <42588003+Mabero@users.noreply.github.com>
2022-11-21 14:53:12 +0100

    redirect crm article

--- next.config.js
+++ next.config.js
@@ -4,24 +4,36 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['wp.xn--byrguiden-72a.no', 'images.squarespace-cdn.com']
+  },
+  async rewrites() {
+    const slugs = await rewrite()
+
+    return slugs.map(({ prefix, slug, catSlug }) => ({
+      source: `${catSlug}/${slug}`,
+      destination: `/${prefix}/${slug}`
+    }))
+  },
+  async redirects() {
+    const slugs = await rewrite()
+
+    const redirects = slugs.map(({ prefix, slug, catSlug }) => ({
+      source: `/${prefix}/${slug}`,
+      destination: `${catSlug}/${slug}`,
+      permanent: true
+    }))
+
+    return [
+      ...redirects,
+      {
+        source: '/index',
+        destination: '/',
+        permanent: true
+      },
+      {
+        source: '/webutvikling/beste-crm-systemer',
+        destination: '/webdesign/beste-crm-systemer',
+        permanent: true
+      }
+    ]
   }
-  // async rewrites() {
-  //   const slugs = await rewrite()
-  //
-  //   return slugs.map(({ prefix, slug }) => ({
-  //     source: `/${slug}`,
-  //     destination: `/${prefix}/${slug}`
-  //   }))
-  // },
-  // async redirects() {
-  //   const slugs = await rewrite()
-  //
-  //   const redirects = slugs.map(({ prefix, slug }) => ({
-  //     source: `/${prefix}/${slug}`,
-  //     destination: `/${slug}`,
-  //     permanent: true
-  //   }))
-  //
-  //   return [...redirects]
-  // }
 }
